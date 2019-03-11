dffd040891b534f685387341fa97eeabe22e6109
Zhicheng WANG <zcwang@thoughtworks.com>
2019-03-11 12:47:39 +0800

    chore: 支持预先渲染

--- scripts/ci/deploy-cn.sh
+++ scripts/ci/deploy-cn.sh
@@ -8,6 +8,16 @@ npm run build
 commitSha=$(git rev-parse --short HEAD)
 commitMessage=$(git log --oneline -n 1)
 
+kill `lsof -t -i :4000` || true
+
+npx http-server-spa dist/woaijiaren index.html 4000 &
+
+rm -fr ./prerender || true
+
+npx prerender mirror -r ./prerender http://localhost:4000/
+
+kill `lsof -t -i :4000` || true
+
 git clone https://asnowwolf:${GITHUB_ACCESS_TOKEN}@github.com/asnowwolf/5aijiaren.com-prebuilt.git prebuilt
 
 cd ./prebuilt
@@ -15,6 +25,8 @@ cd ./prebuilt
 cp -r ../dist/woaijiaren/* .
 
 cp index.html 404.html
+
+cp -r ../prerender/localhost:4000/* .
 
 git add .
 git commit --allow-empty -am "${commitMessage}" || true
