53df5dec4c42e68d1d36cb30983b7231290ea2f4
Zhicheng WANG <zcwang@thoughtworks.com>
2019-03-16 13:44:47 +0800

    fix: 增加一篇文章

--- src/app/article/api/article.model.ts
+++ src/app/article/api/article.model.ts
@@ -1,5 +1,6 @@
 export class ArticleModel {
   id: number;
+  icon: string;
   title: string;
   content: string;
 }
--- src/app/shared/mock/articles.ts
+++ src/app/shared/mock/articles.ts
@@ -3,7 +3,14 @@ declare function require(path: string): string;
 export const articles = [
   {
     id: 1,
+    icon: '',
     title: '叮当家政（通州）开业酬宾',
     content: require('./articles/开业大吉/index.md'),
+  },
+  {
+    id: 2,
+    icon: 'help_outline',
+    title: '常识：怎么做家务可以保护腰椎？',
+    content: require('./articles/怎么做家务可以保护腰椎/index.md'),
   },
 ];
